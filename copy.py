2f5c9a2d43767c07f1e8183d9351c73505c0a2f3
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-03-01 21:16:32 -0500

    Initial commit

---
+++ copy.py
@@ -0,0 +1,32 @@
+import subprocess
+
+infile = open("paths.txt", "r")
+lists = infile.readlines()
+infile.close()
+
+for i in range(len(lists)):
+    lists[i] = lists[i].strip()
+
+
+srcRoot = lists[0] + "\\"
+destRoot = lists[1] + "\\"
+
+argsList = []
+
+i = 2
+while i < len(lists):
+    args = ["robocopy", "/E"]
+    args.append(srcRoot + lists[i])
+    args.append(destRoot + lists[i])
+    j = i
+    i += 1
+    if i < len(lists) and lists[i].startswith('-'):
+        args.append("/XD")
+        while i < len(lists) and lists[i].startswith('-'):
+            args.append(srcRoot + lists[j] + "\\" + lists[i][1:])
+            i += 1
+    argsList.append(args)
+
+    
+for args in argsList:
+    subprocess.run(args)
